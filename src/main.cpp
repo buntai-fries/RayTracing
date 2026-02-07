84d95311070fc2db425a544c1f2cd7eccc3662e4
Buntai-Fries <anuphmm2005@gmail.com>
2026-02-07 09:59:36 +0530

    Rendering ppm Image format.

--- src/main.cpp
+++ src/main.cpp
@@ -2,5 +2,26 @@
 
 int main()
 {
-    
+    int imageWidth = 260;
+    int imageHeight = 260;
+    // PPM Description
+    std::cout << "P3\n" << imageWidth << " " << imageHeight << "\n255\n";
+
+    for (int j = 0; j < imageHeight; j++)
+    {
+        for (int i = 0; i < imageWidth; i++)
+        {
+            auto r = double(i)/(imageWidth-1);
+            auto g = double(j)/(imageHeight-1);
+            auto b = 0.0;
+
+            int ir = int(255.999*r);
+            int ig = int(255.999*g);
+            int ib = int(255.999*b);
+
+            std::cout << ir << " " << ig << " " << ib << "\n";
+        }
+    }
+
+    return 0;
 }
