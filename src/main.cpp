9e0d30619906c81f4a20b24a35690093208ccbe4
Buntai-Fries <anuphmm2005@gmail.com>
2026-02-12 19:20:53 +0530

    Use the ray cast to create image.

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,48 @@
 #include "color.h"
 #include "vec3.h"
+#include "ray.h"
 #include <iostream>
+
+color ray_color(const ray& r) {
+    vec3 unitDirection = unit_vector(r.direction());
+    auto a = 0.5 * (unitDirection.y() + 1.0);
+    return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
+}
 
 int main()
 {
-    int imageWidth = 260;
-    int imageHeight = 260;
-    // PPM Description
-    std::cout << "P3\n" << imageWidth << " " << imageHeight << "\n255\n";
+    int imageWidth = 400;
+    auto aspectRatio = 16.0/9.0;
+    int imageHeight = int(imageWidth / aspectRatio);
+    imageHeight = (imageHeight < 1) ? 1 : imageHeight;
+    // Setting Camera
+    auto viewportWidth = 2.0;
+    auto focalLength = 1.0;
+    auto viewportHeight = viewportWidth * (double(imageWidth)/imageHeight);
+    auto cameraCenter = point3(0, 0, 0);
+    // Calculate the vector across vertically and horizontally
+    auto viewport_u = vec3(viewportWidth, 0, 0);
+    auto viewport_v = vec3(0, -viewportHeight, 0);
+    // Calculate the delta vector across x and y from pixel-pixel
+    auto viewport_del_u = viewport_u / imageWidth;
+    auto viewport_del_v = viewport_v / imageHeight;
+    // Calculate the location of the upper left pixel
+    auto viewUpperLeft = cameraCenter - 
+                         vec3(0, 0, focalLength) - 
+                         viewport_u/2 - 
+                         viewport_v/2;
+    auto pixelLoc = viewUpperLeft + 0.5*(viewport_del_u + viewport_del_v);
 
-    for (int j = 0; j < imageHeight; j++)
-    {
-        for (int i = 0; i < imageWidth; i++)
-        {
-            auto pixel_color = color(double(i)/(imageWidth-1), double(j)/(imageHeight-1), 0);
-            write_color(std::cout, pixel_color);
+    std::cout << "P3" << "\n" << imageWidth << " " << 
+    imageHeight << "\n" << "255" << std::endl;
 
+    for (int j = 0; j < imageHeight; j++) {
+        for (int i = 0; i < imageWidth; i++) {
+            auto pixelCenter = pixelLoc + (i * viewport_del_u) + (j * viewport_del_v);
+            auto rayDirection = pixelCenter - cameraCenter;
+            ray r(cameraCenter, rayDirection);
+            auto pixelColor = ray_color(r);
+            write_color(std::cout, pixelColor);
         }
     }
 
